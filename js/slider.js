9057ae777f35225619e975fc3b13abc874abf2f1
Юлия Панкевич <pankevych.iuliia@MacBook-Air-Ulia.local>
2025-04-01 15:17:35 +0400

    init

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+new Swiper('.hero__slider', {
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  slideToClickedSlide: true,
+  autoplay: {
+    delay: 5000,
+  },
+});
